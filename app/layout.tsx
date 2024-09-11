c755260fc7b2765513982b84b5b249b81695f79d
teplostanski <teplostanski@yandex.ru>
2024-09-11 19:09:39 +0400

    chore: Add favicons

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,64 @@
 import { ReactNode } from 'react';
 import Providers from '../src/components/Providers';
 import Header from '@/src/components/Header';
+import Head from 'next/head';
+
+export const metadata = {
+  title: 'Uncrunch',
+  description: 'indexPageDesc',
+  keywords: 'keywords',
+  icons: {
+    icon: [
+      {
+        rel: 'icon',
+        type: 'image/ico',
+        url: '/favicons/favicon.ico',
+      },
+      {
+        rel: 'icon',
+        type: 'image/png',
+        sizes: '16x16',
+        url: '/favicons/favicon-16x16.png',
+      },
+      {
+        rel: 'icon',
+        type: 'image/png',
+        sizes: '32x32',
+        url: '/favicons/favicon-32x32.png',
+      },
+      {
+        rel: 'apple-touch-icon',
+        sizes: '180x180',
+        url: '/favicons/apple-touch-icon.png',
+      },
+      {
+        rel: 'icon',
+        type: 'image/png',
+        sizes: '192x192',
+        url: '/favicons/android-chrome-192x192.png',
+      },
+      {
+        rel: 'icon',
+        type: 'image/png',
+        sizes: '512x512',
+        url: '/favicons/android-chrome-512x512.png',
+      },
+    ],
+  },
+  manifest: '/manifest.webmanifest',
+}
 
 const RootLayout = ({ children }: { children: ReactNode }) => {
   return (
-    <html lang="en">
+    <html lang="ru">
+      <Head>
+        <link rel="apple-touch-icon" sizes="180x180" href="/favicons/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicons/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicons/favicon-16x16.png" />
+        <link rel="manifest" href="/manifest.webmanifest" />
+        <meta name="theme-color" content="#ffffff" />
+        <title>{metadata.title}</title>
+      </Head>
       <body>
         <Providers>
           <Header />
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,17 +1,15 @@
 // app/components/Header.tsx
 
 import { getServerSession } from 'next-auth';
-import SignOutButton from './SignOutButton'; // Импортируем клиентский компонент
+import SignOutButton from './SignOutButton';
 import { authOptions } from '@/app/api/auth/[...nextauth]/route';
 
 export default async function Header() {
-  // Получаем данные сессии на сервере
   const session = await getServerSession(authOptions);
 
   return (
     <header>
       {session?.user ? (
-        // Если есть сессия, рендерим клиентский компонент
         <SignOutButton />
       ) : (
         <p>Please log in</p>
