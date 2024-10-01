3451d069707e2fc3bf931a10f5e9930bc9481bcf
teplostanski <igor@teplostanski.dev>
2024-10-01 18:12:53 +0400

    feat: Create UserMenu component

--- src/3-widgets/header/ui/Header/Header.tsx
+++ src/3-widgets/header/ui/Header/Header.tsx
@@ -1,15 +1,13 @@
-import SignOutButton from '@/src/6-shared/ui/buttons/SignOutButton'
+//import SignOutButton from '@/src/6-shared/ui/buttons/SignOutButton'
 import { FC, memo } from 'react'
 //import { SignOutButton } from '@/4-features/logout';
 import { UserCard } from '@/src/5-entities/user'
 import { Logo } from '@/src/6-shared/ui/logo'
 //import { RepoInfo } from '@/src/3-widgets/repos';
 import styles from './Header.module.scss'
-import {
-  authOptions,
-  CustomSessionUser,
-} from '@/app/api/auth/authOptions'
+import { authOptions, CustomSessionUser } from '@/app/api/auth/authOptions'
 import { getServerSession } from 'next-auth'
+import UserMenu from '@/src/5-entities/user/ui/UserMenu/UserMenu'
 
 const Header: FC = async () => {
   const session = await getServerSession(authOptions)
@@ -20,14 +18,8 @@ const Header: FC = async () => {
     <div className={styles.headerContainer}>
       <Logo width="48px" height="48px" />
       <div className={styles.userInfo}>
-        {customUser && (
-        <UserCard
-          login={customUser.login!}
-          avatar_url={customUser.image!}
-          name={customUser.name!}
-        />
-      )}
-      <SignOutButton />
+        {customUser && <UserMenu customUser={customUser} />}
+        {/*<SignOutButton />*/}
       </div>
     </div>
   )
--- src/5-entities/user/ui/UserCard/UserCard.tsx
+++ src/5-entities/user/ui/UserCard/UserCard.tsx
@@ -1,16 +1,23 @@
 /* eslint-disable @next/next/no-img-element */
 import React from 'react'
-import { GithubUser } from '../..'
 import styles from './UserCard.module.scss'
+import cn from 'classnames'
 
-interface UserCardProps extends GithubUser {}
+interface UserCardProps {
+  avatar_url: string
+  className?: string
+}
 
-const UserCard: React.FC<UserCardProps> = ({ login, avatar_url, name }) => (
-  <div className={styles.container}>
-    <div className={styles.nameContainer}>
+const UserCard: React.FC<UserCardProps> = ({
+  className,
+  avatar_url,
+  ...props
+}) => (
+  <div className={cn(styles.container, className)} {...props}>
+    {/*<div className={styles.nameContainer}>
       <div className={styles.name}>{name}</div>
       <div className={styles.username}>@{login}</div>
-    </div>
+    </div>*/}
     <img className={styles.avatar} src={avatar_url} alt="User Avatar" />
   </div>
 )
