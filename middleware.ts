75df36a507abd8bbffbcba7822e1cb6bf69750ac
teplostanski <igor@teplostanski.dev>
2024-10-14 20:19:26 +0300

    fix(auth): handle session absence on logout to prevent accessing undefined 'login' property

--- middleware.ts
+++ middleware.ts
@@ -1,25 +1,30 @@
 // middleware.ts
 
-import { NextRequestWithAuth, withAuth } from 'next-auth/middleware';
-import { NextFetchEvent } from 'next/server';
+import { NextRequestWithAuth, withAuth } from 'next-auth/middleware'
+import { NextFetchEvent } from 'next/server'
 
 const authMiddleware = withAuth({
   pages: {
     signIn: '/login',
     signOut: '/login',
   },
-});
+  callbacks: {
+    authorized: ({ token }) => !!token,
+  },
+})
 
-export default function middleware(req: NextRequestWithAuth, event: NextFetchEvent) {
-  // Применяем мидлвар аутентификации
-  const authResponse = authMiddleware(req, event);
+export default function middleware(
+  req: NextRequestWithAuth,
+  event: NextFetchEvent
+) {
+  
+  const authResponse = authMiddleware(req, event)
 
-  // Возвращаем ответ
-  return authResponse;
+  return authResponse
 }
 
 export const config = {
   matcher: [
-    '/((?!login|locales|favicons|api).*)', // Защищаем все маршруты, кроме указанных
+    '/((?!login|locales|favicons|api).*)',
   ],
-};
+}
