c5584b60eb7510c7b16f8eee18a36638d72ccf7e
teplostanski <igor@teplostanski.dev>
2024-10-18 23:23:30 +0300

    feat(middleware): implement user-based redirect

--- middleware.ts
+++ middleware.ts
@@ -1,8 +1,9 @@
 // middleware.ts
-
 import { NextRequestWithAuth, withAuth } from 'next-auth/middleware'
-import { NextFetchEvent } from 'next/server'
+import { NextFetchEvent, NextResponse } from 'next/server'
+import { getToken } from 'next-auth/jwt'
 
+// Добавляем авторизацию через next-auth
 const authMiddleware = withAuth({
   pages: {
     signIn: '/login',
@@ -13,18 +14,50 @@ const authMiddleware = withAuth({
   },
 })
 
-export default function middleware(
+export default async function middleware(
   req: NextRequestWithAuth,
   event: NextFetchEvent
 ) {
-  
-  const authResponse = authMiddleware(req, event)
+  const { pathname } = req.nextUrl
 
-  return authResponse
+  // Исключаем запросы к статике и Next.js путям
+  if (
+    pathname.startsWith('/_next') ||
+    pathname.startsWith('/static') ||
+    pathname.startsWith('/favicon') ||
+    pathname.startsWith('/favicons')
+  ) {
+    return NextResponse.next() // Пропускаем запросы к статике и ресурсам Next.js
+  }
+
+  // Выполняем авторизацию с помощью next-auth
+  const authResponse = authMiddleware(req, event)
+  if (authResponse instanceof NextResponse) {
+    return authResponse // Если требуется редирект по авторизации, выполняем его
+  }
+
+  // Проверяем, авторизован ли пользователь через токен
+  const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET })
+
+  // Если токен отсутствует, пропускаем запрос дальше
+  if (!token || !token.login) {
+    return NextResponse.next()
+  }
+
+  const currentUsername = `~${token.login}`
+  const requestedUsername = pathname.split('/')[1] // Получаем запрашиваемого пользователя
+
+  // Если запрашиваемый пользователь не совпадает с текущим пользователем
+  if (currentUsername !== requestedUsername) {
+    const newPathname = pathname.replace(requestedUsername, currentUsername)
+    const url = req.nextUrl.clone()
+    url.pathname = newPathname
+    return NextResponse.redirect(url) // Выполняем редирект на правильный путь
+  }
+
+  return NextResponse.next() // Продолжаем выполнение, если всё совпадает
 }
 
 export const config = {
-  matcher: [
-    '/((?!login|locales|favicons|api).*)',
-  ],
+  matcher: ['/((?!login|locales|favicons|api|_next|static).*)'],
 }
